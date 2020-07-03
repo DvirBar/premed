add3e32d427b59ebdf7ea9c2875d72d77187174d
Dvir Bartov <dvir.bartov1@gmail.com>
2020-07-03 18:08:32 +0300

    Add error handling middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,29 @@
+  const errorHandler = (err, req, res, next) => {
+    try {
+        // Handling unique field errors
+        if (err.name === 'MongoError' && err.code === 11000) {
+            const modelName = res.locals.model
+            const fieldName = Object.keys(err.keyValue)[0]
+            return res.status(400).send({ msg: `A ${modelName} with this ${fieldName} already exists`})
+        }
+
+        // Handling cast errors
+        if(err.name === 'CastError') {
+            return res.status(400).send({ msg: `Invalid ${err.path}: ${err.value}` })
+        }
+        
+        if(err.errors)
+            return res.status(400).send({ msg: err.message }) 
+
+        // Internal server error that shouldn't be sent to the client
+        res.status(500).send({msg: "Internal server error"})
+        
+        next()
+    } catch (e) {
+        // Change in production
+        return res.status(500).send({ msg: "Internal server error" })
+        // throw e;
+    }
+}
+
+module.exports = errorHandler;
