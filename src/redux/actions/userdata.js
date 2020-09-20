4d51dfafca947df1adeb2e24880c1c9397cdd7ff
Dvir Bartov <dvir.bartov1@gmail.com>
2020-09-20 04:14:36 +0300

    Add user data to redux

---
+++ src/redux/actions/userdata.js
@@ -0,0 +1,148 @@
+import {
+    USER_DATA_LOADING,
+    USER_DATA_SUCCESS,
+    USER_DATA_ERROR,
+    USER_DATA_ADD,
+    USER_DATA_UPDATE_PATHS,
+    USER_DATA_INSERT,
+    USER_DATA_TOGGLE_ENABLED,
+    USER_DATA_DELETE,
+} from './types';
+import axios from 'axios';
+import { getMessage, getError } from './messages';
+
+// Basic types
+export const dataLoad = () => {
+    return {
+        type: USER_DATA_LOADING
+    }
+}
+
+export const dataError = () => {
+    return {
+        type: USER_DATA_ERROR
+    }
+}
+
+// Get one user data
+export const getOneUserData = () => dispatch => {
+    dispatch(dataLoad());
+
+    axios.get('api/userdata/user')
+         .then(res => { 
+             dispatch({
+                type: USER_DATA_SUCCESS,
+                payload: res.data
+            })})
+         .catch(err => {
+             dispatch(dataError());
+             dispatch(getError(err));
+         });
+}
+
+// Get all users data by path
+export const getOneUserData = pathId => dispatch => {
+    dispatch(dataLoad());
+
+    axios.get(`api/userdata/user/${pathId}`)
+         .then(res => { 
+             dispatch({
+                type: USER_DATA_SUCCESS,
+                payload: res.data
+            })})
+         .catch(err => {
+             dispatch(dataError());
+             dispatch(getError(err));
+         });
+}
+
+// Create new user data collection
+export const addUserData = data => dispatch => {
+    // Request body
+    const body = JSON.stringify(data);
+
+    axios.post('api/userdata', body)
+         .then(res => {
+             dispatch ({
+                type: USER_DATA_ADD,
+                payload: res.data
+             })})
+         .catch(err => {
+             dispatch(dataError())
+             dispatch(getError(err))
+            })
+}
+
+// Edit user paths
+export const editUserDataPaths = data => dispatch => {
+    dispatch(dataLoad());
+
+    // Request body
+    const body = JSON.stringify(data);
+
+    axios.put('api/userdata/editpaths', body)
+         .then(res => 
+            dispatch({
+                type: USER_DATA_UPDATE_PATHS,
+                payload: res.data
+             }))
+         .catch(err => {
+             dispatch(dataError())
+             dispatch(getError(err))
+            })
+}
+
+// Insert new data to user data collection
+export const editUserDataPaths = data => dispatch => {
+    dispatch(dataLoad());
+
+    // Request body
+    const body = JSON.stringify(data);
+
+    axios.put('api/userdata/insertdata', body)
+         .then(res => 
+            dispatch({
+                type: USER_DATA_INSERT,
+                payload: res.data
+             }))
+         .catch(err => {
+             dispatch(dataError())
+             dispatch(getError(err))
+            })
+}
+
+// Toggle user data enabled status
+export const toggleEnabled = () => dispatch => {
+    dispatch(dataLoad());
+
+    axios.put('api/userdata/toggleEnabled')
+         .then(res => 
+            dispatch({
+                type: USER_DATA_TOGGLE_ENABLED,
+                payload: res.data
+             }))
+         .catch(err => {
+             dispatch(dataError())
+             dispatch(getError(err))
+            })
+}
+
+
+export const deleteUserData = userId => dispatch => {
+    dispatch(dataLoad());
+
+    axios.delete(`api/userdata/${userId}`)
+         .then(res => {
+             dispatch({
+                type: USER_DATA_DELETE,
+                payload: userId
+             })
+             dispatch(getMessage(res.data))
+         })
+         .catch(err => {
+            dispatch(dataError())
+            dispatch(getError(err))
+         })
+}
+
+
---
+++ src/redux/reducers/userdata.js
@@ -0,0 +1,67 @@
+import {
+    USER_DATA_LOADING,
+    USER_DATA_SUCCESS,
+    USER_DATA_ERROR,
+    USER_DATA_ADD,
+    USER_DATA_UPDATE_PATHS,
+    USER_DATA_INSERT,
+    USER_DATA_TOGGLE_ENABLED,
+    USER_DATA_DELETE,
+} from '../actions/types';
+
+const initialState = {
+    loading: false,
+    data: [],
+}
+
+export default function(state = initialState, action) {
+    const payload = action.payload;
+
+    switch(action.type) {
+        case USER_DATA_LOADING:
+            return {
+                ...state,
+                loading: true
+            }
+
+        case USER_DATA_SUCCESS: 
+            return {
+                ...state,
+                loading: false,
+                data: payload
+            }
+
+        case USER_DATA_ERROR:
+            return {
+                ...state,
+                loading: false,
+            }
+
+        case USER_DATA_ADD:
+            return {
+                ...state,
+                loading: false,
+                data: [...state.data, payload]
+            }
+
+        case USER_DATA_UPDATE_PATHS:
+        case USER_DATA_INSERT:
+        case USER_DATA_TOGGLE_ENABLED:
+            return {
+                ...state,
+                loading: false,
+                data: state.data.map(data => data.user === payload.user 
+                    ? data = payload : data)
+            }
+
+        case USER_DATA_DELETE:
+            return {
+                ...state,
+                loading: false,
+                data: state.data.filter(data => data.user !== payload)
+            }
+
+        default:
+            return state;
+    }
+}
