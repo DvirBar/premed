b07a59750472d720d886da6edfc42d11a28740c4
Dvir Bartov <dvir.bartov1@gmail.com>
2021-05-22 07:43:16 +0300

    Fix bug that caused the program to crushed when a custom group was added to omitted args

--- client/src/components/Profile/user-data/data-block/calc-block/SuggestedCalc/CalcDetails/PayloadInfo/OmittedArgs/OmmitedArgItem.jsx
+++ client/src/components/Profile/user-data/data-block/calc-block/SuggestedCalc/CalcDetails/PayloadInfo/OmittedArgs/OmmitedArgItem.jsx
@@ -1,18 +1,16 @@
 import { Close } from '@material-ui/icons'
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { getGroupById } from '../../../../../../../../../redux/selectors/statsinputs'
+import DisplayGroupName from '../AddedArgs/DisplayGroupName'
 
 function OmmitedArgItem({ arg }) {
-    const group = useSelector(getGroupById(arg))
-
     return (
         <div 
         key={arg}
         className="payload-info__omitted-args__list__item">
             <Close />
             <div>
-                {group.name}
+                <DisplayGroupName 
+                argValue={arg} />
             </div>
         </div>    
     )
