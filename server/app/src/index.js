d4355707d4698292a63bb33097045b406bf8123d
Dvir Bartov <dvir.bartov1@gmail.com>
2023-02-21 02:55:28 +0200

    test

--- server/app/src/index.js
+++ server/app/src/index.js
@@ -23,7 +23,7 @@ dotenv.config({
 })
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(/^(?!\/api\/).+/ ,express.static('../client/build'))
+    // app.use(/^(?!\/api\/).+/ ,express.static('../client/build'))
     app.use(express.static('../client/build'))
 } 
 
